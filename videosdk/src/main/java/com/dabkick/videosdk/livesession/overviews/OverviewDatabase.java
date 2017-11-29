88dc7855b3e3603100ff09ef322b32c2b344e2e1
clevertrevor <trevornemanic@gmail.com>
2017-11-29 09:43:07 -0800

    fix crash from using wrong database reference

--- videosdk/src/main/java/com/dabkick/videosdk/livesession/overviews/OverviewDatabase.java
+++ videosdk/src/main/java/com/dabkick/videosdk/livesession/overviews/OverviewDatabase.java
@@ -1,6 +1,7 @@
 package com.dabkick.videosdk.livesession.overviews;
 
 
+import com.dabkick.videosdk.livesession.AbstractDatabaseReferences;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,14 +18,13 @@ public class OverviewDatabase {
 
     public OverviewDatabase() {
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-
         String overviewPath = OverviewDatabaseReferences.getOverviewReference();
         databaseReference = firebaseDatabase.getReference(overviewPath);
 
         ChildEventListener childEventListener = new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                databaseReference = databaseReference.child(dataSnapshot.getKey());
+                //databaseReference = databaseReference.child(dataSnapshot.getKey());
                 overviewModel = dataSnapshot.getValue(OverviewModel.class);
                 Timber.i("added stage key: %s", overviewModel.getStagedVideoKey());
                 listener.onStageKeyFromDatabaseChanged(overviewModel.getStagedVideoKey());
@@ -40,17 +40,14 @@ public class OverviewDatabase {
             public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
             public void onCancelled(DatabaseError databaseError) {}
         };
+
         databaseReference.addChildEventListener(childEventListener);
-        //createInitialObject();
     }
 
-    private void createInitialObject() {
-        //overviewModel = new OverviewModel(0);
-        //databaseReference.push().setValue(overviewModel);
-    }
 
     public void setStageKey(String newKey) {
-        databaseReference.child(OverviewDatabaseReferences.STAGED_VIDEO_KEY).setValue(newKey);
+        databaseReference.child(AbstractDatabaseReferences.getSessionId()).
+                child(OverviewDatabaseReferences.STAGED_VIDEO_KEY).setValue(newKey);
     }
 
     public String getStagedVideoKey() {
