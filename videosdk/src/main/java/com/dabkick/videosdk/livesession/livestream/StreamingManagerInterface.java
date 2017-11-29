29faf18c6cb07eaba0d63b02356987495aa717e0
gopal <gopal@dabkick.com>
2017-11-29 12:32:54 -0800

    SM Interface added

---
+++ videosdk/src/main/java/com/dabkick/videosdk/livesession/livestream/StreamingManagerInterface.java
@@ -0,0 +1,12 @@
+//package com.dabkick.videosdk.livesession.livestream;
+//
+//import com.twilio.video.VideoView;
+//
+//
+//interface StreamingManagerInterface {
+//
+//    void startStreaming(VideoView videoView);
+//    void stopStreaming(VideoView videoView);
+//    boolean isStreaming();
+//
+//}
